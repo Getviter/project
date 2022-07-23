79fe72390495ce283ee8678f4a397f92238004d8
Getviter <paradero@mail.ru>
2022-07-24 00:51:53 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// //"use strict";
+
+// a = 15;
+// console.log(a);
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+// let storeName = 'Magaz';
+// const storeDescription = {
+//     budget: 10000,
+//     employees: ['alex', 'bob', 'mike'],
+//     products: {
+//         'apple': 5000,
+//         'cola': 3000
+//     },
+//     open: true
+// }
+
+// const result = confirm('are you here?');
+// console.log(result);
+
+// const result = prompt('are you here?', '');
+// console.log(result);
+
+// const result = +prompt('are you here?', '');
+// console.log(typeof(result));
+
+// const answers = [];
+// answers[0] = prompt('ti kto');
+// answers[1] = prompt('ti kak');
+// answers[2] = prompt('ti kogda');
+
+
+const result = prompt('Как вас зовут?');
+alert(`Привет, ${result}`);
